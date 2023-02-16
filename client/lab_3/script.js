7620f5d300b1e998b20fada05e75a68900ebe0fa
ColeDM-2020 <68867747+ColeDM-2020@users.noreply.github.com>
2023-02-16 09:34:45 -0500

    update

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -24,6 +24,7 @@ function updateSlidePosition() {
     slide.classList.add('hidden')
   })
   
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible')
 }
 
@@ -34,16 +35,17 @@ function moveToNextSlide() {
   else{
     slidePosition += 1
   }
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
   // add your code in here for when you click the "prev" button
+  if (slidePosition === 0){
+    slidePosition = 3
+  }
+  else{
+    slidePosition -= 1
+  }
   /*
     add an if statement here that checks
     if you're already at the first index position for an array
