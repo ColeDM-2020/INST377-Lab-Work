22cb35776b81fcec4a700d1199f41c66bd053c9a
ColeDM-2020 <68867747+ColeDM-2020@users.noreply.github.com>
2023-04-13 10:39:07 -0400

    finished

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -1,9 +1,3 @@
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
-
-/* A quick filter that will return something based on a matching input */
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -36,27 +30,52 @@ function getRandomIntInclusive(min, max) {
       return list[index]
     })
   }
-    
-    /*
-      Using the .filter array method, 
-      return a list that is filtered by comparing the item name in lower case
-      to the query in lower case
-  
-      Ask the TAs if you need help with this
-    */
+   
+  function initMap() {
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto
+  }
+   
+  function markerPlace(array, map) {
+    console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+
+    array.forEach((item) => {
+      console.log('markerPlace', item);
+      const {coordinates} = item.geocoded_column_1;
+      
+      L.marker([coordinates[1], coordinates[0]]).addTo(map)
+    })
+  }
   
   async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-    const filterDataButton = document.querySelector('#filter_button');
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate'); // Add a querySelector that targets your filter button here
     const textField = document.querySelector('#resto');
   
     const loadAnimation = document.querySelector('#date_load_animation');
     loadAnimation.style.display = 'none';
     generateListButton.style.display.add = ('hidden');
+
+    const carto = initMap();
+
+    const storedData = localStorage.getItem('storedData')
+    let parsedData = JSON.parse(storedData)
+    if (parsedData?.length > 0) {
+      generateListButton.classList.remove('hidden')
+    }
   
-    let storedList = [];
     let currentList = []; // this is "scoped" to the main event function
     
     /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -67,34 +86,25 @@ function getRandomIntInclusive(min, max) {
 
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
-      storedList = await results.json();
-      if (storedList.length > 0) {
+      const storedList = await results.json();
+      localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList;
+      
+      if (parsedData?.length > 0) {
         generateListButton.classList.remove('hidden')
       }
-  
+
       loadAnimation.style.display = 'none';
-      console.table(storedList); 
+      //console.table(storedList); 
   
     });
   
-    filterDataButton.addEventListener('click', (event) =>{
-      console.log('clicked filterButton');
-  
-      const formData = new FormData(mainForm);
-      const formProps = Object.fromEntries(formData);
-  
-      console.log(formProps);
-      const newList = filterList(currentList, formProps.resto);
-     
-      console.log(newList);
-      injectHTML(newList);
-    })
-  
     generateListButton.addEventListener('click', (event) => {
       console.log('generate new list');
-      currentList = cutRestaurantList(storedList);
+      currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto);
     })
 
     textField.addEventListener('input', (event) => {
@@ -102,6 +112,13 @@ function getRandomIntInclusive(min, max) {
         const newList = filterList(currentList, event.target.value);
         console.log(newList);
         injectHTML(newList);
+        markerPlace(newList, carto);
+    })
+
+    clearDataButton.addEventListener('click', (event) => {
+      console.log('clear browsing data');
+      localStorage.clear();
+      console.log('localStorage Check', localStorage.getItem('storedData'));
     })
 
   }
